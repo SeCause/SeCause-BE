3f11baf25ef88393d348e28b9671d7bea3f69961
dldusgh318 <pololydotoly@gmail.com>
2026-04-30 01:16:05 +0900

    CHORE : 프로젝트 초기 세팅 (#1)

---
+++ src/main/java/SeCause/SeCause_be/global/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package SeCause.SeCause_be.global.config;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI secauseAPI(){
+        Info info = new Info()
+                .title("SeCause API")
+                .description("SeCause API 명세서")
+                .version("1.0");
+
+        String jwtSchemeName = "Bearer Authentication";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server())
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
